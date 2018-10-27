a91c3b54c5d94f1d76e332130f75f884ac01228e
David Rosenblum <david@rosenblum.net>
2018-10-27 13:14:21 -0400

    Tornado server + react

--- server/handlers/main_handler.py
+++ server/handlers/main_handler.py
@@ -2,5 +2,5 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        with open("index.html") as file:
+        with open("../web/build/index.html") as file:
             self.finish(file.read())
